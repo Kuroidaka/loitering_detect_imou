8fb51d5d78bb4968a9e41828d918bcb451076507
Kuroidaka <pcanhgm@gmail.com>
2025-05-11 19:52:24 +0700

    first commit

--- config/env.py
+++ config/env.py
@@ -3,7 +3,9 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     RTSP_URL: str = Field(env="RTSP_URL", default="", description="URL video streaming")
-
+    CAMERA_IP: str = Field(env="CAMERA_IP", default="", description="Camera's ip in local network")
+    CAMERA_USERNAME: str = Field(env="CAMERA_USERNAME", default="", description="username to access camera")
+    CAMERA_PASSWORD: str = Field(env="CAMERA_PASSWORD", default="", description="password to access camera")
     class Config:
         validate_assignment = True
         case_sensitive = True
